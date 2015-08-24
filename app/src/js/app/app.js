3f10fe2ed0b4fb663c8839ed097fa19375e5cebd
Jean-Baptiste Bernus <jbernus@gmail.com>
2015-08-24 21:47:17 +0200

    routeparams cleaned

--- app/src/js/app/app.js
+++ app/src/js/app/app.js
@@ -2,9 +2,6 @@ var podcastApp = angular.module('podcastApp', ['ngSanitize', 'ngRoute', 'podcast
 
 podcastApp.config(['$routeProvider', function ($routeProvider){
 	$routeProvider.
-	when('/', {
-		controller: 'homeCtrl'
-	}).
 	when('/:slug/:id/:url', {
 		templateUrl: 'partials/podcast.html',
 		controller: 'podcastCtrl'
